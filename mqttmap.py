f395665d67713f0faeaaaa07f5d9fc8537448dce
Mihai Stana <mihai.stana@digitalvirgo.com>
2022-08-01 00:04:04 +0200

    mqtt2ble test

--- mqttmap.py
+++ mqttmap.py
@@ -1,7 +1,7 @@
 
 import blemap as bmap
 
-def limit(arg):
+def limit_dpm(arg):
     limitValue = int(arg) * 10
     return bmap.WALLBOX_EPROM["SET_DPM_LIMIT"].format(limit = str(limitValue))
 
@@ -10,8 +10,8 @@ MQTT2BLE = {
     "easywallbox/dpm" : {
         "on" : bmap.WALLBOX_EPROM["SET_DPM_ON"],
         "off" : bmap.WALLBOX_EPROM["SET_DPM_OFF"],
-        "limit" : bmap.WALLBOX_EPROM["GET_DPM_LIMIT"]
-        "limit/" : limit
+        "limit" : bmap.WALLBOX_EPROM["GET_DPM_LIMIT"],
+        "limit/" : limit_dpm,
         "status" : bmap.WALLBOX_EPROM["GET_DPM_STATUS"] #get status
     }
 }
