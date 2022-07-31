05ded2839471217d60167d90298598a56922904b
Mihai Stana <mihai.stana@digitalvirgo.com>
2022-07-31 02:56:02 +0200

    fix commands

--- commands.py
+++ commands.py
@@ -2,34 +2,34 @@
 import easywallbox.blemap as bmap
 
 
-def setUserLimit(limit)
+def setUserLimit(limit):
     
     limitValue = limit * 10
     return bmap.WALLBOX_EPROM["SET_USER_LIMIT"].format(limit = str(limitValue))
     
-def setDpmLimit(limit)
+def setDpmLimit(limit):
     limitValue = limit * 10
     return bmap.WALLBOX_EPROM["SET_DPM_LIMIT"].format(limit = str(limitValue))
 
-def setSafeLimit(limit)
+def setSafeLimit(limit):
     limitValue = limit * 10
     return bmap.WALLBOX_EPROM["SET_SAFE_LIMIT"].format(limit = str(limitValue))
 
 
-def startCharge(delay=0)
+def startCharge(delay=0):
     return bmap.WALLBOX_COMMANDS["START_CHARGE"].format(delay = str(delay))
 
-def stopCharge()
+def stopCharge():
     return bmap.WALLBOX_COMMANDS["STOP_CHARGE"]
 
 
-def setDpmOff()
+def setDpmOff():
     return bmap.WALLBOX_EPROM["SET_DPM_OFF"]
 
-def setDpmOn()
+def setDpmOn():
     return bmap.WALLBOX_EPROM["SET_DPM_ON"]
 
 
-def authBle(pin="0000") #9844 
+def authBle(pin="0000"): #9844 
     return bmap.WALLBOX_BLE["LOGIN"].format(pin = str(pin))
 
