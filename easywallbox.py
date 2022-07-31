72eee5361a54ce93ae36aaf4f4a04d91f5eb1bd6
Mihai Stana <mihai.stana@digitalvirgo.com>
2022-07-31 03:43:05 +0200

    test run

--- easywallbox.py
+++ easywallbox.py
@@ -55,7 +55,7 @@ async def easywallbox(address):
         resp = await client.write_gatt_char(BLUETOOTH_WALLBOX_RX, data, response=True)
         print("BLE AUTH START:", PIN)
         print("BLE AUTH RESPONSE:", resp)
-        await asyncio.sleep(0.5, loop=loop)
+        await asyncio.sleep(0.5, result=b'$BLE,AUTH,OK\n')
         
 
 if __name__ == "__main__":
